dd3d781a1711f98510dabbf1c68fa011d739fbd9
watermelonwuxi <952086169@qq.com>
2015-03-14 10:18:29 +0800

    Update uploader.bat

--- server/uploader.bat
+++ server/uploader.bat
@@ -1,1 +1,7 @@
 @"%~dp0..\local\python27.exe" "%~dpn0.py" && exit /b 0 || (pause && exit /b -1)
+APPID:jisngsu-wuxi
+APPLICATION:江无锡民丰
+HOST:appengine.google.com
+ROLLING BACK THE UPDATE.
+EMAIL:952086169@QQ.com
+PASSWORD FOR wt19950317
